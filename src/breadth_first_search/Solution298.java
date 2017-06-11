d7e599a2326209325d49a96a2c34179cbe3fafe3
Jianyu Zuo <zuowings@gmail.com>
2017-06-10 21:37:56 -0700

    Practice some questions

--- src/breadth_first_search/Solution298.java
+++ src/breadth_first_search/Solution298.java
@@ -1,7 +1,7 @@
 //298. Binary Tree Longest Consecutive Sequence
 
 package breadth_first_search;
-
+/*
 public class Solution298 {
 
     private int largest = -1;   
@@ -41,8 +41,42 @@ public class Solution298 {
                 DC(root.right,1);
             }            
         }
-        
-        
+    }
+}
+*/
+public class Solution298 {
+
+    int globalMax;
+    public int longestConsecutive(TreeNode root) {
+        if(root == null){   return 0;}
+        globalMax = 1;
+        helper(root,1);
+        return globalMax;
+    }
+
+    public void helper(TreeNode root, int curLength){
+
+        if(root.left != null){
+            if(root.val +1 == root.left.val){
+                helper(root.left,curLength+1);
+            }else{
+                globalMax = Math.max(globalMax,curLength);
+                helper(root.left,1);
+            }
+        }else{
+            globalMax = Math.max(globalMax,curLength);
+        }
+
+        if(root.right != null ){
+            if(root.val +1 == root.right.val){
+                helper(root.right,curLength+1);
+            }else{
+                globalMax = Math.max(globalMax,curLength);
+                helper(root.right,1);
+            }
+        }else{
+            globalMax = Math.max(globalMax,curLength);
+        }
     }
 
 }
---
+++ src/google/TreeNode.java
@@ -0,0 +1,11 @@
+package google;
+
+/**
+ * Created by Amos on 2017/6/10.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
