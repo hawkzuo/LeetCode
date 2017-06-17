9c7e3f083e781875f00c8aeb22d451b8e4b20e0f
Jianyu Zuo <zuowings@gmail.com>
2017-06-17 16:24:55 -0700

    Add Q to com.google

---
+++ src/GeneralTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Amos on 2017/7/1.
+ */
+
+public class GeneralTest {
+
+
+    public static void main(String[] args){
+        System.out.println("Tom Dickens , Katy Perry , ".split(" , ").length);
+        System.out.println("Tom Dickens , ".split(" , ").length);
+    }
+
+}
