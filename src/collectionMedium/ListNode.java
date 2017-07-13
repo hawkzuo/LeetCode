6bc24d8b0d085529dbc024bef5a917c48f6c1dfc
Jianyu Zuo <zuowings@gmail.com>
2017-07-12 23:00:46 -0700

    Create Medium Problems Collection
    Create a collection set for reviewing medium questions.
    Order by the question number.

---
+++ src/collectionMedium/ListNode.java
@@ -0,0 +1,12 @@
+package collectionMedium;
+
+/**
+ * Created by Amos on 2017/7/12.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int i) {
+        val = i;
+    }
+}
